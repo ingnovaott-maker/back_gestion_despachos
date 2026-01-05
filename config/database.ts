fa5420f2e906e35e64dd18a3ae0b1668e34fc013
Julio Jimenez <julio.zaor@gmail.com>
2026-01-05 07:51:40 -0500

    feat: Añadir searchPath en la configuración de PostgreSQL

--- config/database.ts
+++ config/database.ts
@@ -42,6 +42,7 @@ const databaseConfig: DatabaseConfig = {
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME')
       },
+      searchPath: [Env.get('SCHEMA', 'sicov'), 'public'],
       migrations: {
         naturalSort: true,
       },
