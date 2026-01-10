2cc309a6361cb921e2d581e64c4bfc93ee95f2a5
Julio Jimenez <julio.zaor@gmail.com>
2026-01-10 09:41:00 -0500

    feat: Actualizar datos del usuario administrador en el seeder

--- database/seeders/5.Usuario.ts
+++ database/seeders/5.Usuario.ts
@@ -9,29 +9,11 @@ export default class UsuariosSeeder extends BaseSeeder {
     await TblUsuarios.createMany([
         {
             nombre: 'Administrador',
-            clave: '$bcrypt$v=98$r=10$cl/YM5O+Cd1YIwp+m5HCKw$qAydh613N6JqkMf0HTWi9GJkQbwdXNM', // Admin123+
-            correo: 'prueba@gmail.com',
-            identificacion: '11111111',
+            clave: '$bcrypt$v=98$r=10$Q2vRX+GrDwa30G8ZYEEstg$34VPuMK3oHOKbzn+ztDtwwWwmYi3CWY', // G3sMov1l+
+            correo: 'administrador@correo.com',
+            identificacion: '999999999',
             idRol: ROLES.ADMINISTRADOR,
-            usuario: '11111111'
-        },
-        {
-            nombre: 'Empresa prueba',
-            clave: '$bcrypt$v=98$r=10$cl/YM5O+Cd1YIwp+m5HCKw$qAydh613N6JqkMf0HTWi9GJkQbwdXNM', // Admin123+
-            correo: 'prueba@gmail.com',
-            identificacion: '800086050',
-            idRol: ROLES.CLIENTE,
-            usuario: '800086050',
-            tokenAutorizado: '34346af4-949d-4f72-bcc6-19d7633b414e'
-        },
-        {
-            nombre: 'Empresa prueba',
-            clave: '$bcrypt$v=98$r=10$cl/YM5O+Cd1YIwp+m5HCKw$qAydh613N6JqkMf0HTWi9GJkQbwdXNM', // Admin123+
-            correo: 'prueba@gmail.com',
-            identificacion: '890100531',
-            idRol: ROLES.CLIENTE,
-            usuario: '890100531',
-            tokenAutorizado: '7d94f935-90d4-429e-b7ec-b3903b2e30ed'
+            usuario: '999999999'
         }
     ])
   }
