38e9a8ee0ddcb709417369cab26458cde5a7ec63
Julio Jimenez <julio.zaor@gmail.com>
2026-05-11 09:24:06 -0500

    feat: simplificar mensajes de error en la autenticación de usuario

--- app/Dominio/Datos/Servicios/ServicioAutenticacion.ts
+++ app/Dominio/Datos/Servicios/ServicioAutenticacion.ts
@@ -39,14 +39,14 @@ export class ServicioAutenticacion {
     const usuario = await this.verificarUsuario(identificacion)
     if (usuario instanceof Usuario) {
       if (!(await this.encriptador.comparar(clave, usuario.clave))) {
-        throw new Exception('Credenciales incorrectas, por favor intente recuperar contraseña con su correo', 400)
+        throw new Exception('Credenciales incorrectas', 400)
       }
       usuario.clave = nuevaClave
       usuario.claveTemporal = false;
       this.servicioUsuario.actualizarUsuario(usuario.id, usuario)
       return;
     }
-    throw new Exception('Credenciales incorrectas, por favor intente recuperar contraseña con su correo', 400)
+    throw new Exception('Credenciales incorrectas', 400)
   }
 
   public async iniciarSesion(usuario: string, contrasena: string): Promise<RespuestaInicioSesion> {
@@ -78,7 +78,7 @@ export class ServicioAutenticacion {
     if (!usuarioVerificado) {
       this.manejarIntentoFallido(registroDeBloqueo);
       throw new Exception(
-        "Credenciales incorrectas, por favor intente recuperar contraseña con su correo",
+        "Credenciales incorrectas",
         400
       );
     }
@@ -88,7 +88,7 @@ export class ServicioAutenticacion {
     ) {
       this.manejarIntentoFallido(registroDeBloqueo);
       throw new Exception(
-        "Credenciales incorrectas, por favor intente recuperar contraseña con su correo",
+        "Credenciales incorrectas",
         400
       );
     } */
@@ -142,7 +142,7 @@ export class ServicioAutenticacion {
   public async verificarUsuario(usuario: string): Promise<Usuario> {
     const usuarioDB = await this.servicioUsuario.obtenerUsuarioPorUsuario(usuario)
     if (!usuarioDB) {
-      throw new Exception('Credenciales incorrectas, por favor intente recuperar contraseña con su correo', 400)
+      throw new Exception('Credenciales incorrectas', 400)
     }
     return usuarioDB
   }
